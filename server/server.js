fe0c381d8b37418180a8eb7e515039a82bf2b293
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-03-26 09:57:58 +0300

    Set up database connection

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Database connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log("MongoDB connected");
+    })
+    .catch(err => {
+      console.error("MongoDB connection error:", err);
+    });
+
+// Start Server
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
